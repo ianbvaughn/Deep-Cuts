e4a98bdf072ece2de1a8e7e28a5e11d7f159e90f
Ian Vaughn <ianbvaughn@gmail.com>
2026-01-12 13:45:31 -0800

    Added CORS support

--- spotify-controller.mjs
+++ spotify-controller.mjs
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import { apiLogger } from './api-logger.mjs';
 import * as spotify from './spotify-model.mjs';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 app.use(apiLogger);
 
 const clientId = process.env.CLIENT_ID;
