ced17aac15b7e7ab8d8b101e04e45c4994fced6d
Ian Vaughn <ianbvaughn@gmail.com>
2026-01-12 16:02:51 -0800

    Edited gitignore, changed logic for  column

--- spotify_dbt/models/silver/silver_api_logs.sql
+++ spotify_dbt/models/silver/silver_api_logs.sql
@@ -5,7 +5,7 @@ select
    endpoint,
    http_method,
    status_code,
-   case when status_code = 200 then 'Success' else 'Failure' end as status,
+   case when status_code >= 200 or status_code <= 399 then 'Success' else 'Failure' end as status,
    response_time_ms,
    response_body,
    ip_address,
