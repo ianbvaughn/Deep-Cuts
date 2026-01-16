8deb637d2b1280251a26a3db7821dcefb733fcf5
Ian Vaughn <ianbvaughn@gmail.com>
2026-01-16 13:44:52 -0800

    passing environment vars for CI via github

--- backend/spotify-controller.test.js
+++ backend/spotify-controller.test.js
@@ -1,29 +1,31 @@
+import 'dotenv/config';
+
 test('Server is responding', async () => {
-   const url = `http://localhost:10000/spotify/artist/Radiohead`;
+   const url = `${process.env.API_BASE_URL}/spotify/artist/Radiohead`;
    const response = await fetch(url);
    expect(response.status).toBe(200);
 });
 
 test('Has artist data', async () => {
-   const url = `http://localhost:10000/spotify/artist/Radiohead`;
+   const url = `${process.env.API_BASE_URL}/spotify/artist/Radiohead`;
    const response = await fetch(url);
    expect((await response.text()).length).toBeGreaterThan(0);
 });
 
 test('Has album data', async () => {
-   const url = `http://localhost:10000/spotify/albums/4Z8W4fKeB5YxbusRsdQVPb`;
+   const url = `${process.env.API_BASE_URL}/spotify/albums/4Z8W4fKeB5YxbusRsdQVPb`;
    const response = await fetch(url);
    expect((await response.text()).length).toBeGreaterThan(0);
 });
 
 test('Has tracks data', async () => {
-   const url = `http://localhost:10000/spotify/tracks/2ix8vWvvSp2Yo7rKMiWpkg`;
+   const url = `${process.env.API_BASE_URL}/spotify/tracks/2ix8vWvvSp2Yo7rKMiWpkg`;
    const response = await fetch(url);
    expect((await response.text()).length).toBeGreaterThan(0);
 });
 
 test('Has detailed track data', async () => {
-   const url = `http://localhost:10000/spotify/track/4mRSbPLnOm54ttkTYvxxSY`;
+   const url = `${process.env.API_BASE_URL}/spotify/track/4mRSbPLnOm54ttkTYvxxSY`;
    const response = await fetch(url);
    expect((await response.text()).length).toBeGreaterThan(0);
 });
