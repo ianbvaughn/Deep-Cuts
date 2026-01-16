46490a514dca27d56a6d81ba41f1f580182f5bab
Ian Vaughn <ianbvaughn@gmail.com>
2026-01-16 12:36:41 -0800

    Wrote tests for controller

---
+++ backend/spotify-controller.test.js
@@ -0,0 +1,29 @@
+test('Server is responding', async () => {
+   const url = `http://localhost:10000/spotify/artist/Radiohead`;
+   const response = await fetch(url);
+   expect(response.status).toBe(200);
+});
+
+test('Has artist data', async () => {
+   const url = `http://localhost:10000/spotify/artist/Radiohead`;
+   const response = await fetch(url);
+   expect((await response.text()).length).toBeGreaterThan(0);
+});
+
+test('Has album data', async () => {
+   const url = `http://localhost:10000/spotify/albums/4Z8W4fKeB5YxbusRsdQVPb`;
+   const response = await fetch(url);
+   expect((await response.text()).length).toBeGreaterThan(0);
+});
+
+test('Has tracks data', async () => {
+   const url = `http://localhost:10000/spotify/tracks/2ix8vWvvSp2Yo7rKMiWpkg`;
+   const response = await fetch(url);
+   expect((await response.text()).length).toBeGreaterThan(0);
+});
+
+test('Has detailed track data', async () => {
+   const url = `http://localhost:10000/spotify/track/4mRSbPLnOm54ttkTYvxxSY`;
+   const response = await fetch(url);
+   expect((await response.text()).length).toBeGreaterThan(0);
+});
