6d9dd5c0603fce800462e089062e75d2109472ba
Ian Vaughn <ianbvaughn@gmail.com>
2026-01-21 12:06:31 -0800

    Updated pg-db logging, dbt to use local Supabase for modelling

--- backend/pg-db.mjs
+++ backend/pg-db.mjs
@@ -1,8 +1,8 @@
 import 'dotenv/config'
 import { Pool } from 'pg'
 
-console.log('Creating pool with CONNECTION_STRING:', process.env.CONNECTION_STRING ? 'SET' : 'NOT SET');
-console.log('First 20 chars:', process.env.CONNECTION_STRING?.substring(0, 20));
+console.log('Info: CONNECTION_STRING ', process.env.CONNECTION_STRING ? 'SET' : 'NOT SET');
+console.log('Info: First 3 chars of CONNECTION_STRING:', process.env.CONNECTION_STRING?.substring(0, 3), '***');
 
 const pool = new Pool({
    connectionString: process.env.CONNECTION_STRING,
@@ -11,18 +11,8 @@ const pool = new Pool({
    }
 });
 
-console.log('Pool created successfully');
+console.log('Info: Pool created successfully');
 
 export const query = (text, values) => pool.query(text, values);
 export const getClient = () => pool.connect();
 export { pool };
-
-/*
-const pool = new Pool({
-   user: 'ivaughn',
-   password: 'password',
-   host: 'localhost',
-   port: 5432,
-   database: 'spotify_db'
-});
-*/
